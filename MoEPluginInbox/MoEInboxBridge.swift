f0e8abd0247d425d99c8b78ba91752ca242bca63
Rakshitha <>
2022-08-09 11:07:51 +0530

    Updated Inbox library structure

--- MoEPluginInbox/MoEInboxBridge.swift
+++ MoEPluginInbox/MoEInboxBridge.swift
@@ -9,42 +9,45 @@ import Foundation
 import MoEngageInbox
 import MoEPluginBase
 
-@objc final public class MoEInboxBridge: NSObject, MoEPluginUtils {
+@objc final public class MoEInboxBridge: NSObject, MoEInboxUtils {
     @objc public static let sharedInstance = MoEInboxBridge()
     
     private override init() {
     }
     
     @objc public func getInboxMessages(_ inboxDict: [String: Any], completionHandler: @escaping(([String: Any]) -> Void)) {
-        if let controller = getController(inboxDict: inboxDict) {
-            controller.getInboxMessages(inboxDict: inboxDict, completionHandler: completionHandler)
+        guard let identifier = MoEInboxBridge.fetchIdentifier(attribute: inboxDict) else { return }
+        MOInbox.sharedInstance.getInboxMessages(forAppID: identifier) { inboxMessages, _ in
+            let payload = MoEInboxBridge.getInboxPayload(inboxMessages: inboxMessages, identifier: identifier)
+            completionHandler(payload)
         }
+        
     }
     
     @objc public func trackInboxClick(_ inboxDict: [String: Any]) {
-        if let controller = getController(inboxDict: inboxDict) {
-            controller.trackInboxClick(inboxDict: inboxDict)
-        }
+        guard let identifier = MoEInboxBridge.fetchIdentifier(attribute: inboxDict),
+              let campaignID = MoEInboxBridge.getCampaignIdForStats(inboxDict: inboxDict)
+        else { return }
+        
+        MOInbox.sharedInstance.trackInboxClick(withCampaignID: campaignID, forAppID: identifier)
     }
     
     @objc public func deleteInboxEntry(_ inboxDict: [String: Any]) {
-        if let controller = getController(inboxDict: inboxDict) {
-            controller.deleteInboxEntry(inboxDict: inboxDict)
-        }
+        guard let identifier = MoEInboxBridge.fetchIdentifier(attribute: inboxDict),
+              let campaignID = MoEInboxBridge.getCampaignIdForStats(inboxDict: inboxDict)
+        else { return }
+        
+        MOInbox.sharedInstance.removeInboxMessage(withCampaignID: campaignID, forAppID: identifier)
     }
     
     @objc public func getUnreadMessageCount(_ inboxDict: [String: Any], completionHandler: @escaping(([String: Any]) -> Void)) {
-        if let controller = getController(inboxDict: inboxDict) {
-            controller.getUnreadMessageCount(inboxDict: inboxDict, completionHandler: completionHandler)
-        }
-    }
         
-    private func getController(inboxDict: [String: Any]) -> MoEInboxPluginController? {
-        if let appID = MoEInboxBridge.fetchIdentifier(attribute: inboxDict),
-           let controller = MoEInboxPluginCoordinator.sharedInstance.getPluginCoordinator(identifier: appID) {
-            return controller
+        guard let identifier = MoEInboxBridge.fetchIdentifier(attribute: inboxDict) else { return }
+        
+        MOInbox.sharedInstance.getUnreadNotificationCount(forAppID: identifier) { count, _ in
             
+            let payload = MoEInboxBridge.getUnreadCountPayload(count: count, identifier: identifier)
+            completionHandler(payload)
         }
-        return nil
     }
 }
