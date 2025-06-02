dc30562e64428158946a2881b763339da10fb853
Rakshitha . <rakshitha@moengage.com>
2025-06-02 10:11:41 +0530

    MOEN-36562: Accessibility support added

--- MoEngagePluginInbox/MoEngagePluginInboxConstants.swift
+++ MoEngagePluginInbox/MoEngagePluginInboxConstants.swift
@@ -28,8 +28,8 @@ struct MoEngagePluginInboxConstants {
         static let deepLink = "deepLink"
         static let richLanding = "richLanding"
         static let screenName = "screenName"
-        static let accessibilityData = "accessibilityData"
-        static let accessibilityLabel = "label"
+        static let accessibility = "accessibility"
+        static let accessibilityText = "text"
         static let accessibilityHint = "hint"
     }
 }
