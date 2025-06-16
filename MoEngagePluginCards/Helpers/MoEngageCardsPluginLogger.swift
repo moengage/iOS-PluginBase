043e77106f6e6eccc18fc85211283f997130e5c9
Soumya Ranjan Mahunt <133663939+msoumya-engg-sdk@users.noreply.github.com>
2025-06-16 12:19:29 +0530

    MOEN-39045: Update API usage label

--- MoEngagePluginCards/Helpers/MoEngageCardsPluginLogger.swift
+++ MoEngagePluginCards/Helpers/MoEngageCardsPluginLogger.swift
@@ -49,7 +49,7 @@ public enum MoEngagePluginCardsLogger {
             attribute: hybridData
         ) {
             MoEngageCoreHandler.globalQueue.async  {
-                guard let instance = MoEngageSDKInstanceProvider.getSDKInstance(appId: identifier) else { return }
+                guard let instance = MoEngageSDKInstanceProvider.getSDKInstance(workspaceId: identifier) else { return }
                 Self.sdkInstance = instance
                 log(
                     message, withPayload: hybridData, withType: type, inInstance: instance,
